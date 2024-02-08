7c7a22a99b63f0ffb984fde01fe2adc12106f57f
vDk3 <vDk3@users.noreply.github.com>
2024-02-08 20:09:10 +0100

    Update bindings.js

--- bindings.js
+++ bindings.js
@@ -34,6 +34,7 @@ var fs = require('fs'),
     version: process.versions.node,
     bindings: 'bindings.node',
     try: [
+      ['module_root', 'bin', 'win32-x64-119'],
       // node-gyp's linked version in the "build" dir
       ['module_root', 'build', 'bindings'],
       // node-waf and gyp_addon (a.k.a node-gyp)
